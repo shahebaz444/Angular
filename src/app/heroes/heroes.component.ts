f49ae30df838a66555693f4e3833262278221717
shahebaz <shahebaz.khan444@gmail.com>
2019-01-04 22:48:19 +0530

    First Commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  constructor(private heroService: HeroService) {}
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  ngOnInit() {
+    this.gerHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  gerHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
+  }
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+export const Heroes: Hero[] = [
+  { id: 1, name: 'Thor' },
+  { id: 2, name: 'Hulk' },
+  { id: 3, name: 'IronMan' },
+  { id: 4, name: 'Cap' },
+  { id: 5, name: 'BlackWidow' },
+  { id: 6, name: 'WandaMaximoff' },
+  { id: 7, name: 'AntMan' },
+  { id: 8, name: 'DrStrange' },
+  { id: 9, name: 'BlackPanther' },
+  { id: 10, name: 'SpiderMan' }
+];
+export class MockHeroes {}
